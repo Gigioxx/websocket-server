f0911eaeaf00c32827cf7f512f00c259e7835928
Gigiox <guillermo.casanova.b@gmail.com>
2022-03-10 15:25:49 -0300

    Show connection and disconnection messages on client

--- public/js/socket-client.js
+++ public/js/socket-client.js
@@ -1,3 +1,23 @@
-
+// HTML References
+const lblOnline = document.querySelector('#lblOnline');
+const lblOffline = document.querySelector('#lblOffline');
 
 const socket = io();
+
+socket.on('connect', () => {
+
+    console.log('Connected');
+
+    lblOffline.style.display = 'none';
+    lblOnline.style.display = '';
+    
+});
+
+socket.on('disconnect', () => {
+
+    console.log('Disconnected');
+
+    lblOnline.style.display = 'none';
+    lblOffline.style.display = '';
+
+});
