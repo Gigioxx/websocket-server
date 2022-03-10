b69d1b75ce87f150b69cc1b842e2c67a5c54d277
Gigiox <guillermo.casanova.b@gmail.com>
2022-03-10 15:05:52 -0300

    Installation and configuration of socket.io

--- models/server.js
+++ models/server.js
@@ -6,6 +6,8 @@ class Server {
     constructor() {
         this.app = express();
         this.port = process.env.PORT;
+        this.server = require('http').createServer( this.app );
+        this.io = require('socket.io')( this.server );
 
         this.paths = { }
 
@@ -32,7 +34,7 @@ class Server {
     }
 
     listen() {
-        this.app.listen( this.port, () => {
+        this.server.listen( this.port, () => {
             console.log( 'Server running in port:', this.port );
         });
     }
